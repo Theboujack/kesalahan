a90a6bb089cbe7ab8d97a652d6bca0e3c7942dd1
Sya444 <159936097+Theboujack@users.noreply.github.com>
2024-12-17 19:05:42 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// Set the date we're counting down to
+const countDownDate = new Date("Dec 18, 2024 10:30:59").getTime();
+
+// Update the countdown every 1 second
+const interval = setInterval(function() {
+  const now = new Date().getTime();
+  const distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="countdown"
+  document.getElementById("countdown").innerHTML =
+   hours + "h " + minutes + "m " + seconds + "s ";
+
+  // If the countdown is finished, display a message
+  if (distance < 0) {
+    clearInterval(interval);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
