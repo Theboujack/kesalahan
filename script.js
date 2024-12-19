85d4298eaf5277a649c2112fc7b974d6eb9e9d98
Sya444 <159936097+Theboujack@users.noreply.github.com>
2024-12-19 21:30:43 +0700

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-const countDownDate = new Date("Dec 18, 2024 10:30:59").getTime();
+const countDownDate = new Date("Dec 21, 2024 12:00:59").getTime();
 
 // Update the countdown every 1 second
 const interval = setInterval(function() {
